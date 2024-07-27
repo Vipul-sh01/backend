94b6464c31ce0d2904a622b3e11cf5a200637a2e
Vipul-sh01 <vipulsharma6021@gmail.com>
2024-07-27 20:22:29 +0530

    second comit

--- server.js
+++ server.js
@@ -3,11 +3,33 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT||3000;
 
-app.get('/', (req, res)=>{
-    res.send("vipul sharma");
+app.get('/book', (req, res)=>{
+    const book = [
+        {
+            id: 1,
+            title: 'English',
+            content: 'english',
+        },
+        {
+            id: 2,
+            title: 'English',
+            content: 'operating system',
+        },
+        {
+            id: 3,
+            title: 'English',
+            content: 'javacript',
+        },
+        {
+            id: 4,
+            title: 'English',
+            content: 'flutter',
+        },
+    ]
+    res.send(book);
 })
 app.listen(PORT, ()=>{
-    console.log(`server ready on http://localhost:${PORT}`);
+    console.log(`server ready on http://192.168.1.3:${PORT}`);
 })
 
 
